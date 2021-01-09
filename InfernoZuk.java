11ecee3f05984c6f00bd4780ecd5404e4d5d43fb
hillsaannie54 <77194620+hillsaannie54@users.noreply.github.com>
2021-01-09 11:39:13 +0000

    Create InfernoZuk.java

---
+++ InfernoZuk.java
@@ -0,0 +1,24 @@
+# Inferno ZUK Practice INSTANT 3 JAD
+Inferno zuk practice instant 3 jad, triple jad practice, AUTO PRAY FLICK, AUTO SWITCH RUNELITE OPENOSRS
+
+
+INFERNO INSTANT ZUK PRACTICE PRIVATE SERVER RSPS
+
+https://www.moparscape.org/rsps-list/server/inferno-zuk-practice-osrs-rsps
+
+Inferno ZUK practice - triple JAD - 3 JADS- INSTANT TELEPORT - practice zuk osrs rsps now
+
+
+inferno-zuk-practice-osrs-rsps RSPS
+
+
+
+AUTO PRAY SWITCH
+
+
+AUTO PRAY CLICK
+
+FOR RUNELITE AND OPENOSRS
+
+net.runelite.client
+net.openosrs.client
